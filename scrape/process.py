85994bfe5ac21a45d8dae0644f33d05d65c39396
Max Ovsiankin <max@ovsankin.com>
2016-06-07 18:17:52 -0700

    Add foods.json with all the data we want

--- scrape/process.py
+++ scrape/process.py
@@ -3,13 +3,13 @@ import json
 import string
 NUM_FOODS = 8789
 
-foods = {}
+foods = []
 old_used = None
 with open('used.json', 'r') as f:
     old_used = json.load(f)
 
 for i in old_used:
-  d = pq(filename="../usndb/{}.html".format(i))
+  d = pq(filename="../../usndb/{}.html".format(i))
   vn = d("#view-name").text()
   vn = vn.replace("Full Report (All Nutrients):", "").strip()[7:]
   name = vn.split(', ')[0]
@@ -17,8 +17,28 @@ for i in old_used:
     'name': name,
     'group': d(".value").eq(0).text()
   }
-  tab = d(".nutlist table")
-  print(tab)
+  tab = d(".nutlist table, .nutlist-hscroll table")
+  rows = tab.find("tr.odd")
+  data = []
+  print(i, name, "({})".format(food["group"]))
+  for row in list(rows.items()):
+      txt = []
+      for td in list(row.find("td").items())[:3]:
+        txt.append(td.text())
+    #   print(' ', txt)
+      data.append(txt)
+  name_map = {
+    'Energy': 'calories',
+    'Protein': 'protein',
+    'Total lipid (fat)': 'fat',
+    'Carbohydrate, by difference': 'carbs',
+    'Fiber, total dietary': 'fiber',
+    'Sugars, total': 'sugar'
+  }
+  for d in data:
+      if d[0] in name_map.keys():
+          food[name_map[d[0]]] = d[2] + '' + d[1]
+  foods.append(food)
 
 with open('foods.json', 'w') as f:
     json.dump(foods, f)
