4af9cf736b5328d2b923b205d086b4fc478d757e
oriklein195 <klein.ori98@gmail.com>
2016-06-15 16:19:33 -0700

    Infile SinglyLinkedList and LinkedListNode

--- project/project/HashTable.h
+++ project/project/HashTable.h
@@ -10,8 +10,209 @@
 #define HashTable_h
 
 #include "Food.h"
-#include "SinglyLinkedList.h"
 #include <string>
+#include <iostream>
+
+using namespace std;
+
+template<class Type>
+class LinkedListNode{
+private:
+	Type data;
+public:
+	LinkedListNode* next;
+	int key;
+	LinkedListNode(Type t, int k){
+		data = t;
+		next = nullptr;
+		key = k;
+	}
+
+	void setData(Type d){ data = d; }
+
+	int getKey() { return key; }
+
+	Type getData(){ return data; }
+};
+
+/*
+Template class that creates a singly linked list with the ability to add a node anywhere, remove from anywhere,
+empty, extract data from node.
+head is an LinkedListNode pointer that points to the first node in the list, without holding data
+rear is an LinkedListNode pointer that points to the last node in the list, without holding data
+index of first node is 0, index of last node is size -1.
+*/
+template<class Type>
+class SinglyLinkedList{
+private:
+	int size;
+	LinkedListNode<Type>* head;
+	LinkedListNode<Type>* rear;
+public:
+	/**
+	Creates an instance of a SinglyLinkedList
+	*/
+	SinglyLinkedList(){
+		size = 0;
+		head = nullptr;
+		rear = nullptr;
+	}
+	/**
+	adds a node to beginning of the list, head will point to that node
+	*/
+	void addFirst(Type data){
+		LinkedListNode<Type>* newNode = new LinkedListNode<Type>(data);
+		newNode->next = head;
+		head = newNode;
+		size++;
+		if (size == 1)
+			rear = newNode;
+	}
+	/**
+	adds a node after the node specified at that index
+	*/
+	void addAfter(Type data, int index){
+		LinkedListNode<Type>* newNode = new LinkedListNode<Type>(data);
+		if (index > size){
+			return;
+		}
+		if (index == size){
+			addLast(newNode);
+			return;
+		}
+		if (index == 0){
+			addFirst(newNode);
+			return;
+		}
+		int i = 0;
+		LinkedListNode<Type>* pPre = nullptr;
+		newNode->next = head;
+		while (newNode->next != nullptr && i < index){
+			pPre = newNode;
+			newNode = newNode->next;
+		}
+		pPre->next = newNode;
+		size++;
+	}
+	/**
+	adds a node at the size index
+	*/
+	void addLast(Type data, int key){
+		LinkedListNode<Type>* newNode = new LinkedListNode<Type>(data, key);
+		if (size == 0){
+			head = newNode;
+			rear = newNode;
+			newNode->next = nullptr;
+			size++;
+			return;
+		}
+		rear->next = newNode;
+		newNode->next = nullptr;
+		rear = newNode;
+		size++;
+	}
+	/**
+	deletes node from existance
+	*/
+	void removeFirst(){
+		if (size < 1)
+			return;
+		LinkedListNode<Type>* pLoc = head;
+		head = head->next;
+		delete pLoc;
+		size--;
+		if (size == 1)
+			rear = head;
+	}
+	/**
+	deletes node from existance at indicated index
+	*/
+	bool remove(int key){
+		if (size <= 0){
+			return false;
+		}
+		LinkedListNode<Type>* pLoc = head;
+		LinkedListNode<Type>* pPre = nullptr;
+		while (pLoc->next != nullptr && pLoc->getKey() != key){
+			pPre = pLoc;
+			pLoc = pLoc->next;
+		}
+		if (pLoc->getKey() != key)
+			return false;
+		if (pLoc == head){
+			head = pLoc->next;
+			size--;
+			return true;
+		}
+		pPre->next = pLoc->next;
+		if (pPre->next == nullptr)
+			rear = pPre;
+		delete pLoc;
+		size--;
+		return true;
+	}
+	/**
+	extracts data from node at specified index
+	*/
+	Type get(int key){
+		LinkedListNode<Type>* pLoc = head;
+		while (pLoc->next != nullptr && pLoc->getKey() != key){
+			pLoc = pLoc->next;
+		}
+		return pLoc->getData();
+	}
+	int find(Type data){
+
+	}
+	void print(){
+		if (size == 0)
+		{
+			cout << "\n{ }\n";
+			return;
+		}
+		LinkedListNode<Type>* p = head;
+		LinkedListNode<Type>* q = head;
+		cout << "\n{ ";
+		cout << head->getData();
+		if (head->next)
+			cout << ", ";
+		else
+			cout << " ";
+		while (q)
+		{
+			p = q;
+			if (p != head)
+			{
+				cout << p->getData();
+				if (p->next)
+					cout << ", ";
+				else
+					cout << " ";
+			}
+			q = p->next;
+		}
+		cout << "}\n";
+	}
+	/**
+	returns size
+	*/
+	int getSize(){
+		return size;
+	}
+	/**
+	deletes all nodes
+	*/
+	void empty(){
+		int s = size;
+		for (int i = 0; i < s; i++){
+			removeFirst();
+		}
+		head = nullptr;
+		rear = nullptr;
+	}
+
+};
+
 
 //*****************************************************************
 // Hash Table objects store a fixed number of Linked Lists.
