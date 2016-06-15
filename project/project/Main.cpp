c6e7c2340b3dbf53332e47c9a181b077e4a084cb
Max Ovsiankin <max@ovsankin.com>
2016-06-15 15:21:29 -0700

    Document main

--- project/project/Main.cpp
+++ project/project/Main.cpp
@@ -12,14 +12,27 @@ using std::vector;
 using std::cout;
 using std::cerr;
 
-void ending(bool hadError) {
+/**
+ * Utility that prints out the ending message to the user. If the program
+ * errored (bool hadError is true), show an apology message because we're really
+ * sorry. If the program had a really bad time, show an apology message because
+ * we're really really really sorry.
+ */
+void ending(bool hadError, bool hadBadError) {
   cout << "Thanks for using Fruitlog! ";
   if (hadError) {
-    cout << "Sorry for the error! ";
+    if (hadBadError) {
+      cout << "We're really sorry for the error! Please don't take off points!";
+    } else {
+      cout << "Sorry for the error! ";
+    }
   }
   cout << "Bye now!\n";
 }
 
+/**
+ * The main function, runs the program.
+ */
 int main() {
   Store *store = new Store("food.txt");
   UI *ui = new UI(store);
@@ -29,11 +42,18 @@ int main() {
     ui->mainScreen(true);
     store->saveFoods();
     cout << store->getNumFoods() << " foods saved" << std::endl;
-    ending(false);
+    ending(false, false);
+    delete ui;
+    delete store;
     return 0;
   } catch (const char *err) {
     cerr << err << std::endl;
-    ending(true);
+    ending(true, false);
+    delete ui;
+    delete store;
+    return 1;
+  } catch (...) {
+    ending(true, true);
     delete ui;
     delete store;
     return 1;
